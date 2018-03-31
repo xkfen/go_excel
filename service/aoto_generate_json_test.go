85964222019e9ac5d8e05cb1f250e139480c9abd
xiekaifen <xiekaifen@qiyuanfin.com>
2018-03-31 15:50:06 +0800

    测试一些反射例子

--- service/aoto_generate_json_test.go
+++ service/aoto_generate_json_test.go
@@ -3,6 +3,8 @@ package service
 import (
 	"testing"
 	"fmt"
+	"reflect"
+	"strings"
 )
 
 type UserName struct {
@@ -10,7 +12,10 @@ type UserName struct {
 	Name string
 }
 
-
+type BankInfo struct {
+	Name string `json:"name"`
+	CardNum string `json:"card_num"`
+}
 
 func TestGetTagName(t *testing.T){
 	oneStepTest(func() {
@@ -24,5 +29,29 @@ func TestProduceStructTag(t *testing.T) {
 	oneStepTest(func() {
 		json := ProduceStructTag(name, "json")
 		fmt.Println(json)
+	})
+}
+
+// 测试动态参数利用反射生成对应结构体
+func TestReflect2Struct(t *testing.T){
+	oneStepTest(func() {
+
+		// 利用反射查看slice
+		var s []*interface{}
+		slice := reflect.TypeOf(s)
+		fmt.Println(slice)
+		// 利用反射看结构体
+
+		var name UserName
+		model := reflect.TypeOf(name)
+		fmt.Println(model)
+
+		//var obj []interface{}
+		bins := []BankInfo{}
+		m := reflect.TypeOf(bins)
+		fmt.Println(m)
+		//if strings.Contains(m.String(), "Bank"){
+		//	obj = []m
+		//}
 	})
 }
